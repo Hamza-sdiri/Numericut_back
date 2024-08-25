f599d2b7267e0eb25cbd03f5f935c49951b6d361
Hamza-sdiri <hamza.sdiri@gmail.com>
2024-08-25 14:55:07 +0100

    fix:data base connection

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
-mongoose.connect('mongodb://localhost/mydatabase', {
+mongoose.connect('mongodb+srv://user1:TOj4uPuYuYt4Wepu@cluster0.jfm1r.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
