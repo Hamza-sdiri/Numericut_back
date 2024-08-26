61930ec656ea9a359203b9e38d73f2b05f791778
Mezni Oubeid <oubeid.mezni@esprit.tn>
2024-08-26 10:50:46 +0100

    chore: Update MongoDB connection configuration

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+require("dotenv").config();
+
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
-const port = 3000;
-mongoose.connect('mongodb+srv://user1:TOj4uPuYuYt4Wepu@cluster0.jfm1r.mongodb.net/', {
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => {
-        console.log('Connected to MongoDB successfully!');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
+  })
+  .then(() => {
+    console.log("Connected to MongoDB successfully!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
     
 app.get('/', (req, res) => {
     res.send('Hello, World!');
