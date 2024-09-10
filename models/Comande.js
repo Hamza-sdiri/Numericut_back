b12946f59fb50c59ed2e5c9d647208cc978ae758
Mezni Oubeid <oubeid.mezni@esprit.tn>
2024-09-10 20:57:25 +0100

    Refactor Comande.js model to change customer type to String

--- models/Comande.js
+++ models/Comande.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const ComandeSchema = new mongoose.Schema(
   {
     customer: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      type: String,
       required: true,
     },
     totalPrice: {
